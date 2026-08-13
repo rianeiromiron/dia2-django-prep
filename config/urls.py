688929a454b67cb09a78ea2619b7a7d4bd0710bc
Rianeiro <rianeiromiron@gmail.com>
2026-08-12 18:54:38 -0600

    Add root URL redirect to /admin/ for convenience

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -10,6 +11,7 @@ router.register(r"tramites", TramiteViewSet)
 router.register(r"comentarios", ComentarioViewSet)
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/admin/", permanent=False)),
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
